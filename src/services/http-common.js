ab982f1f59a1e15270809b110277f577d57d9e13
vipgirisim <info@vipgirisim.com.tr>
2021-02-15 17:59:42 +0300

    d
    d

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://vipfal.herokuapp.com/admin/",
+  baseURL: "https://vipfal.herokuapp.com/api/",
   headers: {
     "Content-type": "application/json",
     'Access-Control-Allow-Origin': '*'
