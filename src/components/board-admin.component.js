daed7e39832963a369cf4068a0af47cf53c3f879
vipgirisim <info@vipgirisim.com.tr>
2021-02-17 03:33:32 +0300

    hh
    hh

--- src/components/board-admin.component.js
+++ src/components/board-admin.component.js
@@ -1,112 +1,14 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
+import { IntlProvider } from 'react-intl';
+import Layout from './Layout';
+import messages from './messages';
+import '../styles/App.scss';
 
-import UserService from "../services/user.service";
-import AddTutorial from "./Admin/AddTutorial"
-import Tutorial from "./Admin/tutorial"
-import TutorialList from "./Admin/tutorials-list"
-import Uyeekle from "./register.component"
-
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
-import Burclar from './Admin/Burclar'
-export default class BoardAdmin extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      content: ""
-    };
-  }
-
-  componentDidMount() {
-    UserService.getAdminBoard().then(
-      response => {
-        this.setState({
-          content: response.data
-        });
-      },
-      error => {
-        this.setState({
-          content:
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString()
-        });
-      }
-    );
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>{this.state.content}</h3>
-
-        </header>
-
-
-<ProSidebar>
-  <Menu >
-    <MenuItem>Admin Menusu</MenuItem>
-    <SubMenu title="Components">
-      <MenuItem>Component 1</MenuItem>
-      <MenuItem>Component 2</MenuItem>
-    </SubMenu>
-  </Menu>
-</ProSidebar>
-       {/*  <div className="container">
-          <h3> <h3> Admin Ekleme Paneli </h3></h3>
-
-          <header className="jumbotron">
-
-          <Uyeekle/>
-
-          </header>
-
-          <h3> <h3>Moderatör Ekleme Paneli</h3></h3>
-          <header className="jumbotron">
-          <Uyeekle/>
-
-
-          </header>
-          <h3> <h3>Üye Ekleme Paneli</h3></h3>
-          <header className="jumbotron">
-           <Uyeekle/>
-
-          </header>
-          <h3> <h3>Havuz Paneli</h3></h3>
-          <header className="jumbotron">
-            <h3> <h3>s</h3></h3>
-
-          </header>
-
-
-          <h3> <h3>Burç Ekleme Paneli</h3></h3>
-          <header className="jumbotron">
-            <h3> <h3>s</h3></h3>
-
-          </header>
-
-          <h3> <h3>İndirim Paneli</h3></h3>
-          <header className="jumbotron">
-            <h3> <h3>s</h3></h3>
-
-          </header>
-
-          <h3> <h3>Duyuru  Paneli</h3></h3>
-          <header className="jumbotron">
-           
-            <AddTutorial/>
-          <TutorialList/>
- 
-          </header>
-       
-        </div> */}
-
-       </div>
-
-    );
-  }
+export default function BoardAdmin() {
+  const [locale, setLocale] = useState('en');
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+    <Layout setLocale={setLocale} />
+  </IntlProvider>
+  )
 }
