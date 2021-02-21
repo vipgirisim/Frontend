093688a5c88b0d034506f8cd2f31d8315b1f4638
vipgirisim <info@vipgirisim.com.tr>
2021-02-22 01:44:47 +0300

    gg
    gg

--- src/views/Sistemislemler.js
+++ src/views/Sistemislemler.js
@@ -20,7 +20,26 @@ function Sistemislemler() {
         <Row>
           <Col md="12">
             <Card>
-
+            <Table className="tablesorter" responsive>
+                <thead className="text-primary">
+                  <tr>
+                    <th>Başlık</th>
+                    <th>Duyuru Adı</th>  
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Başlık 1</td>
+                    <td>Duyuru 1</td> 
+                  </tr>
+                  <tr>
+                  <td>Başlık 2</td>
+                    <td>Duyuru 2</td> 
+                  </tr>
+             
+                </tbody>
+              </Table>
+          
               <CardHeader>
                 <CardTitle tag="h4">Duyuru Menüsü </CardTitle>
               </CardHeader>
@@ -41,60 +60,6 @@ function Sistemislemler() {
                 <CardTitle tag="h4">Burçlar Menüsü</CardTitle>
               </CardHeader>
 
-              <Table className="tablesorter" responsive>
-                <thead className="text-primary">
-                  <tr>
-                    <th>Name</th>
-                    <th>Country</th>
-                    <th>City</th>
-                    <th className="text-center">Salary</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Dakota Rice</td>
-                    <td>Niger</td>
-                    <td>Oud-Turnhout</td>
-                    <td className="text-center">$36,738</td>
-                  </tr>
-                  <tr>
-                    <td>Minerva Hooper</td>
-                    <td>Curaçao</td>
-                    <td>Sinaai-Waas</td>
-                    <td className="text-center">$23,789</td>
-                  </tr>
-                  <tr>
-                    <td>Sage Rodriguez</td>
-                    <td>Netherlands</td>
-                    <td>Baileux</td>
-                    <td className="text-center">$56,142</td>
-                  </tr>
-                  <tr>
-                    <td>Philip Chaney</td>
-                    <td>Korea, South</td>
-                    <td>Overland Park</td>
-                    <td className="text-center">$38,735</td>
-                  </tr>
-                  <tr>
-                    <td>Doris Greene</td>
-                    <td>Malawi</td>
-                    <td>Feldkirchen in Kärnten</td>
-                    <td className="text-center">$63,542</td>
-                  </tr>
-                  <tr>
-                    <td>Mason Porter</td>
-                    <td>Chile</td>
-                    <td>Gloucester</td>
-                    <td className="text-center">$78,615</td>
-                  </tr>
-                  <tr>
-                    <td>Jon Porter</td>
-                    <td>Portugal</td>
-                    <td>Gloucester</td>
-                    <td className="text-center">$98,615</td>
-                  </tr>
-                </tbody>
-              </Table>
             </Card>
           </Col>
           <Col md="12">
