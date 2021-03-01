05aecd35d596b9673f40ac7b18dac20d593d65e3
vipgirisim <info@vipgirisim.com.tr>
2021-03-01 03:46:14 +0300

    dd
    dd

--- src/views/TableList1.js
+++ src/views/TableList1.js
@@ -14,6 +14,9 @@ import {
 } from "reactstrap";
 
  
+import userImg from '../assets/user.png';
+import { Link } from 'react-router-dom';
+
 export default class TableList1 extends React.Component {
   constructor(props) {
     super(props);
@@ -40,16 +43,17 @@ componentDidMount() {
                 {data.length > 0 ? 
                     data.map((user, i) => {                        
                         return (
-                            <div className="col-lg-6" key={user._id.toString()}>
-                                <div className="card" style={{ marginBottom: "20px"}}>
-                                    <div className="card-header text-left">{user.name}</div>
-                                    <div className="card-body text-left">
-                                        <div className="row">
-                                            <div className="col-lg-3">
-                                                 {/* <Link to={"users/"+user._id}>
-                                                    <button className="btn btn-outline-dark btn-sm">View User</button>
-                                                </Link> */}
-                                            </div>   
+                          <div className="col-lg-6" key={user._id.toString()}>
+                          <div className="card" style={{ marginBottom: "20px"}}>
+                              <div className="card-header text-left">{user.name}</div>
+                              <div className="card-body text-left">
+                                  <div className="row">
+                                      <div className="col-lg-3">
+                                          <img className="img-thumbnail" style={{marginBottom: "10px"}} src={userImg} alt="user"/><br/>
+                                          <Link to={"users/"+user._id}>
+                                              <button className="btn btn-outline-dark btn-sm">View User</button>
+                                          </Link>
+                                      </div>   
                                             <div className="col-lg-9">
                                                 <ul className="list-group">
                                                     <li className="list-group-item"><b>Adi Soyadi </b>: {user.username}</li>
--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -1,157 +1,133 @@
- 
-import React from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router';
 
-// reactstrap components
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardText,
-  FormGroup,
-  Form,
-  Input,
-  Row,
-  Col,
-} from "reactstrap";
+class AddUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            name: "",
+            email: "",
+            phone: "",
+            job: "",
+            company: "",
+            age: "",
+            city: "",
+            redirect: false
+        }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+        this.handleJobChange = this.handleJobChange.bind(this);
+        this.handleCompanyChange = this.handleCompanyChange.bind(this);
+        this.handleAgeChange = this.handleAgeChange.bind(this);
+        this.handleCityChange = this.handleCityChange.bind(this);
+        this.addUser = this.addUser.bind(this);
+      }
+    
+      handleNameChange(e) {
+        this.setState({name: e.target.value})
+      }
+    
+      handleEmailChange(e) {
+        this.setState({email: e.target.value})
+      }
 
-function UserProfile() {
-  return (
-    <>
-      <div className="content">
-        <Row>
-          <Col md="8">
-            <Card>
-              <CardHeader>
-                <h5 className="title">Admin Ekle</h5>
-              </CardHeader>
-              <CardBody>
-                <Form>
-                  <Row>
-                    <Col className="pr-md-1" md="5">
-                      <FormGroup>
-                        <label>Company (disabled)</label>
-                        <Input
-                          defaultValue="Creative Code Inc."
-                          disabled
-                          placeholder="Company"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-md-1" md="3">
-                      <FormGroup>
-                        <label>Username</label>
-                        <Input
-                          defaultValue="michael23"
-                          placeholder="Username"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="4">
-                      <FormGroup>
-                        <label htmlFor="exampleInputEmail1">
-                          Email address
-                        </label>
-                        <Input placeholder="mike@email.com" type="email" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="6">
-                      <FormGroup>
-                        <label>First Name</label>
-                        <Input
-                          defaultValue="Mike"
-                          placeholder="Company"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="6">
-                      <FormGroup>
-                        <label>Last Name</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Last Name"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="12">
-                      <FormGroup>
-                        <label>Address</label>
-                        <Input
-                          defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                          placeholder="Home Address"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="4">
-                      <FormGroup>
-                        <label>City</label>
-                        <Input
-                          defaultValue="Mike"
-                          placeholder="City"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-md-1" md="4">
-                      <FormGroup>
-                        <label>Country</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Country"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="4">
-                      <FormGroup>
-                        <label>Postal Code</label>
-                        <Input placeholder="ZIP Code" type="number" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="8">
-                      <FormGroup>
-                        <label>About Me</label>
-                        <Input
-                          cols="80"
-                          defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                            that two seat Lambo."
-                          placeholder="Here can be your description"
-                          rows="4"
-                          type="textarea"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                </Form>
-              </CardBody>
-              <CardFooter>
-                <Button className="btn-fill" color="primary" type="submit">
-                  Save
-                </Button>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col md="4">
-          
-          </Col>
-        </Row>
-      </div>
-    </>
-  );
+      handlePhoneChange(e) {
+        this.setState({phone: e.target.value})
+      }
+
+      handleJobChange(e) {
+        this.setState({job: e.target.value})
+      }
+
+      handleCompanyChange(e) {
+        this.setState({company: e.target.value})
+      }
+
+      handleAgeChange(e) {
+        this.setState({age: e.target.value})
+      }
+
+      handleCityChange(e) {
+        this.setState({city: e.target.value})
+      }
+      
+      addUser(event) {
+        event.preventDefault();
+        const userAdd = {
+            name: this.state.name,
+            email: this.state.email,
+            phone: this.state.phone,
+            job: this.state.job,
+            company: this.state.company,
+            age: this.state.age,   
+            city: this.state.city        
+        }
+        console.log(userAdd)
+        axios.post('http://localhost:4000/api/users/add', userAdd)
+        .then(res => { 
+            console.log(res);
+            this.setState({ redirect: this.state.redirect === false });
+        })
+        .catch(err => { console.log(err) });
+      }
+    
+    render() {
+        return (
+            <div className="container" style={{marginTop: "50px"}}>
+            <form onSubmit={this.addUser} method="user">
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label text-left">Name</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" onChange={this.handleNameChange} name="name" value={this.state.name}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label text-left">Email</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" onChange={this.handleEmailChange} name="email" value={this.state.email}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label text-left">Phone</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" onChange={this.handlePhoneChange} name="phone" value={this.state.phone}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label text-left">Job</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" onChange={this.handleJobChange} name="job" value={this.state.job}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label text-left">Company</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" onChange={this.handleCompanyChange} name="company" value={this.state.company}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label text-left">Age</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" onChange={this.handleAgeChange} name="age" value={this.state.age}/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label text-left">City</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" onChange={this.handleCityChange} name="city" value={this.state.city}/>
+                    </div>
+                </div>
+                <hr/>
+                <div style={{marginLeft: "0px"}} className="row">
+                    <button type="submit" className="btn btn-warning" style={{marginLeft: "0px"}}>Add User</button>
+                </div>
+            </form>
+           
+          </div>
+        );
+    }
 }
 
-export default UserProfile;
+export default AddUser;
