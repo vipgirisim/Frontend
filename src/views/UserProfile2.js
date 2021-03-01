10f03ed4e05860d12dc7d28ecb123ca161bec4d7
vipgirisim <info@vipgirisim.com.tr>
2021-03-01 04:49:39 +0300

    dd

--- src/views/UserProfile2.js
+++ src/views/UserProfile2.js
@@ -91,7 +91,7 @@ export default class Register extends Component {
         password: this.state.password,      
     }
     console.log(userAdd)
-    axios.post('http://localhost:8080/api/admin/falciekle', userAdd)
+    axios.post('http://localhost:8080/api/admin/modekle', userAdd)
     .then(res => { 
         console.log(res);
         this.setState({ redirect: this.state.redirect === false });
