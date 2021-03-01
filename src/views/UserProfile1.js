8d0c196e7ef0913ef9cbdb605bc8199397abbbb5
vipgirisim <info@vipgirisim.com.tr>
2021-03-02 00:20:29 +0300

    dd
    dd

--- src/views/UserProfile1.js
+++ src/views/UserProfile1.js
@@ -1,133 +1,189 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router';
+import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
+import axios from "axios";
 
-class AddUser extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            name: "",
-            email: "",
-            phone: "",
-            job: "",
-            company: "",
-            age: "",
-            city: "",
-            redirect: false
-        }
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePhoneChange = this.handlePhoneChange.bind(this);
-        this.handleJobChange = this.handleJobChange.bind(this);
-        this.handleCompanyChange = this.handleCompanyChange.bind(this);
-        this.handleAgeChange = this.handleAgeChange.bind(this);
-        this.handleCityChange = this.handleCityChange.bind(this);
-        this.addUser = this.addUser.bind(this);
-      }
-    
-      handleNameChange(e) {
-        this.setState({name: e.target.value})
-      }
-    
-      handleEmailChange(e) {
-        this.setState({email: e.target.value})
-      }
+ 
+const required = value => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
 
-      handlePhoneChange(e) {
-        this.setState({phone: e.target.value})
-      }
+const email = value => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
 
-      handleJobChange(e) {
-        this.setState({job: e.target.value})
-      }
+const vusername = value => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The username must be between 3 and 20 characters.
+      </div>
+    );
+  }
+};
 
-      handleCompanyChange(e) {
-        this.setState({company: e.target.value})
-      }
+const vpassword = value => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The password must be between 6 and 40 characters.
+      </div>
+    );
+  }
+};
 
-      handleAgeChange(e) {
-        this.setState({age: e.target.value})
-      }
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.addUser = this.addUser.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
 
-      handleCityChange(e) {
-        this.setState({city: e.target.value})
-      }
-      
-      addUser(event) {
-        event.preventDefault();
-        const userAdd = {
-            name: this.state.name,
-            email: this.state.email,
-            phone: this.state.phone,
-            job: this.state.job,
-            company: this.state.company,
-            age: this.state.age,   
-            city: this.state.city        
-        }
-        console.log(userAdd)
-        axios.post('http://localhost:4000/api/users/add', userAdd)
-        .then(res => { 
-            console.log(res);
-            this.setState({ redirect: this.state.redirect === false });
-        })
-        .catch(err => { console.log(err) });
-      }
-    
-    render() {
-        return (
-            <div className="container" style={{marginTop: "50px"}}>
-            <form onSubmit={this.addUser} method="user">
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label text-left">Name</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" onChange={this.handleNameChange} name="name" value={this.state.name}/>
-                    </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label text-left">Email</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" onChange={this.handleEmailChange} name="email" value={this.state.email}/>
-                    </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label text-left">Phone</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" onChange={this.handlePhoneChange} name="phone" value={this.state.phone}/>
-                    </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label text-left">Job</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" onChange={this.handleJobChange} name="job" value={this.state.job}/>
-                    </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label text-left">Company</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" onChange={this.handleCompanyChange} name="company" value={this.state.company}/>
-                    </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label text-left">Age</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" onChange={this.handleAgeChange} name="age" value={this.state.age}/>
-                    </div>
-                </div>
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label text-left">City</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" onChange={this.handleCityChange} name="city" value={this.state.city}/>
-                    </div>
-                </div>
-                <hr/>
-                <div style={{marginLeft: "0px"}} className="row">
-                    <button type="submit" className="btn btn-warning" style={{marginLeft: "0px"}}>Add User</button>
-                </div>
-            </form>
-           
-          </div>
-        );
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      successful: false,
+      message: ""
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+
+
+  addUser(event) {
+    event.preventDefault();
+    const userAdd = {
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password,      
     }
-}
+    console.log(userAdd)
+    axios.post('http://localhost:8080/api/admin/modekle', userAdd)
+    .then(res => { 
+        console.log(res);
+        this.setState({ redirect: this.state.redirect === false });
+    })
+    .catch(err => { console.log(err) });
+  }
 
-export default AddUser;
+ 
+
+  render() {
+    return (
+      <div className="col-md-12">
+        <div className="card card-container">
+          <img
+            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            alt="profile-img"
+            className="profile-img-card"
+          />
+
+          <Form
+            onSubmit={this.addUser}
+            ref={c => {
+              this.form = c;
+            }}
+          >
+            {!this.state.successful && (
+              <div>
+                <div className="form-group">
+                  <label htmlFor="username">Adı soyadı</label>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    validations={[required, vusername]}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChangeEmail}
+                    validations={[required, email]}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password">Şifre</label>
+                  <Input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                    validations={[required, vpassword]}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <button className="btn btn-primary btn-block">Kayıt</button>
+                </div>
+              </div>
+            )}
+
+            {this.state.message && (
+              <div className="form-group">
+                <div
+                  className={
+                    this.state.successful
+                      ? "alert alert-success"
+                      : "alert alert-danger"
+                  }
+                  role="alert"
+                >
+                  {this.state.message}
+                </div>
+              </div>
+            )}
+            <CheckButton
+              style={{ display: "none" }}
+              ref={c => {
+                this.checkBtn = c;
+              }}
+            />
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
